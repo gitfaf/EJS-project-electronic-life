dd56d6fef6343c3e6bfd32de0a4f9273edbbd52a
Git Faf <git.faf@gmail.com>
2017-08-21 00:50:16 +0530

    [NODE] carve out require-grapher

--- node/index.js
+++ node/index.js
@@ -1,9 +1,4 @@
-let path = require('path');
-var MODULE = require('module');
-var _require = MODULE.prototype.require;
-MODULE.prototype.require = function (name) {
-    console.log(`${path.basename(__filename)} required ${name}.`);
-    return _require.call(this, name);
-}
+require('./require-grapher')();
+
 const world = require('./world');
 world.run();
--- node/world.js
+++ node/world.js
@@ -69,7 +69,7 @@ const legend = {
 
 function run () {
     let world = new World(randomMap(), legend);
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 3; i++) {
         world.turn();
         console.log(world.toSring());
     }
